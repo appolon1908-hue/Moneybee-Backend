ff473de8d4ecf0df67fd3aec3b9ab0990d03f7d2
Ralph Appolon <appolon1908@gmail.com>
2026-08-23 10:44:35 -0400

    test: initialize API v2 test environment

--- tests/test_api_v2.py
+++ tests/test_api_v2.py
@@ -1,3 +1,9 @@
+import os
+
+os.environ.setdefault("APP_ENV", "test")
+os.environ.setdefault("DATABASE_URL", "sqlite+aiosqlite:///./test-moneybee.db")
+os.environ.setdefault("LOCAL_AUTH_BYPASS", "true")
+
 from fastapi.testclient import TestClient
 
 from app.main import app
