8b0bc23ccb27d0ce7c7f1736f50bc68fe316373e
Ralph Appolon <appolon1908@gmail.com>
2026-08-23 14:34:17 -0400

    chore: use Python 3.13 async generator typing

--- app/core/database.py
+++ app/core/database.py
@@ -29,6 +29,6 @@ AsyncSessionLocal = async_sessionmaker(
 )
 
 
-async def get_db() -> AsyncGenerator[AsyncSession, None]:
+async def get_db() -> AsyncGenerator[AsyncSession]:
     async with AsyncSessionLocal() as session:
         yield session
